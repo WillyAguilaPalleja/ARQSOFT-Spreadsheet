5a303f39b5257604739861f224d2c95f4decd9a7
WillyAguilaPalleja <guillemaguilaipalleja@gmail.com>
2023-12-23 12:09:12 +0100

    Started with read_command from Controller and send_command in UI

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ from entities.spreadsheet import UserInterface
 
 if __name__ == "__main__":
     ui = UserInterface()
-    ui.send_command("create 3 3")
+    ui.send_command()
--- entities/spreadsheet.py
+++ entities/spreadsheet.py
@@ -1,7 +1,10 @@
+import time
 from enum import Enum
 from typing import List
 
 from entities.argument import Cell
+from exceptions.exceptions import BadCommandException
+from utils import help_message
 
 
 class AvailableCommandsEnum(str, Enum):
@@ -10,6 +13,7 @@ class AvailableCommandsEnum(str, Enum):
     E = "E"
     L = "L"
     S = "S"
+    Q = "Q"
 
 
 class Spreadsheet:
@@ -30,11 +34,21 @@ class UserInterface:
             UserInterface._instance = self
             self.controller = SpreadsheetFactory.create_spreadsheet_controller()
 
-    def send_command(self) -> str:
+    def send_command(self) -> None:
         """
         @summary: Sends the command the user has input to the SpreadsheetController.
         @return: The command typed in by the user.
         """
+        user_wants_to_quit = False
+        while not user_wants_to_quit:
+            try:
+                command = input("Enter a command: ")
+                read_command = self.controller.read_command(command)
+                if not read_command:
+                    user_wants_to_quit = True
+            except BadCommandException as exception:
+                print(exception.message)
+                print(help_message)
 
 
 class SpreadsheetController:
@@ -54,12 +68,38 @@ class SpreadsheetController:
         @raise BadCommandException: Raises if the command the user has input is not valid.
         @raise SpreadsheetLocationException: Raises if any spreadsheet was found in path_name or the file did not exist.
         """
+        command_splitted = command.split(" ")
+        try:
+            match command_splitted[0]:
+                case AvailableCommandsEnum.RF:
+                    # Read commands from a file
+                    pass
+                case AvailableCommandsEnum.C:
+                    return self.create_spreadsheet()
+                case AvailableCommandsEnum.E:
+                    return self.edit_cell(
+                        cell_coordinate=command_splitted[1],
+                        new_cell_content=command_splitted[2],
+                    )
+                case AvailableCommandsEnum.L:
+                    return self.load_spreadsheet(path_name=command_splitted[1])
+                case AvailableCommandsEnum.S:
+                    return self.save_spreadsheet(path_name=command_splitted[1])
+                case AvailableCommandsEnum.Q:
+                    return self.exit()
+                case _:
+                    raise BadCommandException()
+        except IndexError:
+            raise BadCommandException(
+                message="The command input is not valid: not enough arguments were given"
+            )
 
     def create_spreadsheet(self) -> Spreadsheet:
         """
         @summary: Creates a spreadsheet using the method create_spreadsheet from SpreadsheetCreation.
         @return: Spreadsheet.
         """
+        pass
 
     def load_spreadsheet(self, path_name: str) -> Spreadsheet:
         """
@@ -67,6 +107,7 @@ class SpreadsheetController:
         @param path_name: Path where the spreadsheet to be loaded is located.
         @return: Spreadsheet.
         """
+        pass
 
     def save_spreadsheet(self, path_name: str) -> None:
         """
@@ -75,6 +116,7 @@ class SpreadsheetController:
         @return: None.
         @raise SpreadsheetLocationException: Raises if any spreadsheet was found in path_name or the file did not exist.
         """
+        pass
 
     def edit_cell(self, cell_coordinate: str, new_cell_content: str) -> None:
         """
@@ -82,13 +124,17 @@ class SpreadsheetController:
         @param cell_coordinate: Coordinate of the cell where to modify its content.
         @param new_cell_content: New content of the cell.
         @return: None
-
         """
+        pass
 
-    def exit(self) -> None:
+    @staticmethod
+    def exit() -> None:
         """
         @summary: Close the application.
         """
+        print("Closing the application")
+        time.sleep(1)
+        return None
 
 
 class SpreadsheetFactory:
